41fd634a268b4a58b2bd0d4ffba6c98081add361
keren <kerenhappuc4@gmail.com>
2023-11-15 15:22:24 +0200

    1

---
+++ Exercice.js
@@ -0,0 +1,102 @@
+// let random = Math.random() * 100 + 1;
+
+//  let magical =  Math.round(random);
+
+// do {
+//     while (i = 0 ;i < 5 ; i++) 
+
+//     let nombre = prompt("devine un nombre")
+//     if (nombre == magical) {
+//         alert("you win");
+//     }
+//     else if (nombre >= 0) {
+//         alert("come again")
+//     }
+//     else if (nombre <= 20) {
+//         alert("almost")
+//     }
+
+// }
+
+
+// const Array = [0, 10 ]
+// let tab = []
+// for (let i = 0; i < array.length; i++) {
+// console.log(array);
+// tab.push = (array)
+
+// }
+// tab (Array[i]) {
+// console.log(tab);
+
+// }
+// Array(arrayLength)
+
+
+
+// console.log( i > 0, i <20);
+
+// // let nombre = ("nombre > 0 && nombre < 20")
+// 
+//          min = nombre(0);
+//          max = nombre(20);
+//          return Math.random(nombrealeatoir) * (max - min) + min;
+
+//          }
+
+/* ***essaie***
+// bouton.addEventListener('click', function(){
+//     titre.innerHTML= input.value
+// let titre = document.querySelector('.h1')
+// let input= document.querySelector('#taskinput')
+// let bouton= document.querySelector('.bouton')
+// })**/
+
+
+// let tasklist = document.querySelector("#tasklist")
+// let taskinput = document.querySelector('#taskinput')
+// let bouton = document.querySelector('bouton')
+
+// bouton.addEventListener('click', function () {
+    
+//     if (input == "") {
+//         return;
+//     }
+//     let lu = document.querySelector('ul')
+
+let bouton = document.queryselector"#bouton"
+
+bouton.addEventlistener("click", function(){
+    let input = document.queryselector(".input")
+    input.value
+    
+    let h1= document.getElementsByClassName(".h1")
+    h1.innerthtml = h1.innerthtml + "" + input.value
+    input.push(newtext)
+input.pop
+   
+})
+
+
+
+
+
+
+
+
+
+/*bouton.addEventlistener("click", function(){
+    let input = document.queryselector(".input")
+    input.value
+    let h1= document.queryselector(".h1")
+    h1.innerthtml = h1.innerthtml + "" + input.value
+
+})
+ 
+
+
+
+// input.addEventlistener("clic", function(event))
+// input.innerthtml= 
+
+    
