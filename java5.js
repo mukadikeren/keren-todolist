19c5e282933d528c96ceca74106906e162803eb2
keren <kerenhappuc4@gmail.com>
2023-11-15 11:57:46 +0200

    java.js

---
+++ java5.js
@@ -0,0 +1,92 @@
+
+
+
+
+
+
+
+// function productSign(n, m) {
+//     let nombre =(n*m)
+//     if (nombre > 0) {
+//       alert("le signe doit etre positif")
+//     }
+//     else if (nombre < 0 ){
+//       alert("le singne doit etre negatif")
+//     }
+//       else {
+//         alert("le signe etre nul")
+//     }
+//       console.log(productSign(n, m))
+//      
+
+// let taille
+// let tab = []
+// do {
+//   taille = parseInt(prompt("entrez nombre"))
+//   console.log(taille)
+//   if (isNaN(taille)) {
+//     alert("n'afffiche pas en caractere")
+//   }
+// }
+// while (isNaN(nombre))
+// for (let i = 0; i < taille; i++) {
+//   let nombre
+
+// }
+// do {
+//   if (isNaN(nombre))
+//     alert("n'affiche pas le caractere")
+// }
+// while (isNaN);
+// tab.push(nombre)
+
+// console.log(tab);
+// console.log(taille);
+// console.log(ordre);
+
+// ***tableau menu***
+// let tab = []
+// let menu = prompt(`selecteur number 
+// ----------------
+// 1 list task
+// 2 add task
+// 3 remove task
+// 4 completed
+// 5 exit
+// `)
+
+// switch (menu) {
+//     case "1":
+//         console.log("list")
+//         break;
+//     case "2": 
+//         console.log("add")
+    
+//         break;
+//     case "3":
+//         console.log("remove")
+//         break
+//     case "4":
+//         console.log("completed");
+//         break
+
+// }
+
+
+
+
+
+
+// const SISE= 8
+// const generate_notes = new Array (size).fill(0).map((v,k) => k + 1) 
+
+// function carre(notes){
+//   let tab = []
+//   for (let i = 0; i < array.length; i++) {
+// console.log(carre(notes));
+//     tab (notes[i] )
+//   }
+//     console.log(tab);
+//   }
+
+// carre(generate_notes)
